a41de24b2a30daad8969db42d4c97d8e135a1168
jpal91 <pallanschja@gmail.com>
2023-07-21 11:25:56 -0400

    added threading for a placeholder

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import time
+import threading
 from dotenv import load_dotenv
 from src import VirusTotal, URLHaus
 load_dotenv()
@@ -30,8 +32,44 @@ def main():
     else:
         report = u_haus.get_report(sys.argv[1])
     
-    print(report)
+    # print(report)
+    return report
+
+class MainThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.report = None
+    
+    def run(self):
+        self.report = main()
+
+class WaitingThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.done = False
+    
+    def run(self):
+
+        while not self.done:
+            for i in range(1, 4):
+                print('  Scanning' + '.' * i, end='\r')
+                time.sleep(0.5)
+            print(' ' * 14, end='\r')
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main_thread = MainThread()
+    waiting_thread = WaitingThread()
+
+    waiting_thread.start()
+    main_thread.start()
+
+    main_thread.join()
+
+    waiting_thread.done = True
+
+    waiting_thread.join()
+    print(main_thread.report)
     
